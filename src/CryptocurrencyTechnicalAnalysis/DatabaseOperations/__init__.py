29433690f923f5e7134e1d43dff2a5c6d42d6b01
Hunter Dale <guyyatsu@gmail.com>
2024-04-10 19:53:56 +0000

    Format SQL statement with indentation.

--- src/CryptocurrencyTechnicalAnalysis/DatabaseOperations/__init__.py
+++ src/CryptocurrencyTechnicalAnalysis/DatabaseOperations/__init__.py
@@ -18,18 +18,18 @@ def WriteHistoricBars(database, symbol, timestamp, _high, _low, _open, _close):
     )
     database.commit()
 
-def DataframeCuration(database, _limit: int = 100):
-    """ Dataset Curation
-    With a back-log of data secured, we construct a pandas dataframe from 
-    the top &number of most recent bars collected and make it available
-    as a member of HistoricPerformanceBars.
-
-    Now that we've caught up on any missed information and skimmed what we
-    need, we can finally close the database connection and call it a day.
-    """
+def DataframeCuration(database, _limit: int = 360):
     database = connect(database)
-    dataframe = querysql("SELECT * FROM historic_price_data ORDER BY epoch DESC LIMIT ?;", database,
+    query = """
+        SELECT * FROM (
+            SELECT * FROM historic_price_data
+            ORDER BY epoch DESC
+            LIMIT ?
+        ) ORDER BY epoch ASC;
+    """
+    dataframe = querysql(
+        query,
+        database,
         params=[_limit]
     ); database.close()
-
     return dataframe
