66d445b0e97c1661b4ceabf54894d5d589b01dd4
Hunter Dale <guyyatsu@gmail.com>
2024-04-22 23:25:30 -0400

    Bring in datetime constants as required by PriceData.

--- src/CryptocurrencyTechnicalAnalysis/_constants/__init__.py
+++ src/CryptocurrencyTechnicalAnalysis/_constants/__init__.py
@@ -1,1 +1,22 @@
 #!/bin/python3
+from datetime import timedelta, datetime
+from Lab93CryptographyAPI import CryptogramAPI
+
+# List out the Year Month and Day for todays date as a default fallback.
+default_start = [
+    int(number) for number in \
+        datetime.strftime( datetime.now(),
+                           "%Y %-m %-d"     )\
+                .split()
+]
+def date_counter(days, date):
+    """
+    Give a list of consecutive datetime objects
+    going backwards from the current date.
+    """
+    step, _list = 1, []
+    while step <= days:
+        _list.append(
+            date - timedelta(days=step)
+        ); step += 1
+    return _list
